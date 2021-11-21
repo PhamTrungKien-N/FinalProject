7b394c8ce10211dae7449f53c7ab367164a29cca
PTK <18520957@gm.uit.edu.com>
2021-11-21 17:53:14 +0700

    Final update Darkmode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import NotesList from "./Components/NoteLists";
 import Search from './Components/Search';
+import Header from './Components/Header';
 const App = () => {
     const [notes, setNotes] = useState([{
         id: nanoid(),
@@ -25,6 +26,23 @@ const App = () => {
     },
 ]);
     const [searchText, SetsearchText] = useState('');
+    const [darkMode, setDarkMode] =useState(false);
+    useEffect(() =>{
+        const savedNotes = JSON.parse(
+            localStorage.getItem('react-notes-app-data')
+        );
+
+        if (savedNotes) {
+            setNotes(savedNotes);
+        }
+    },[])
+
+    useEffect(() =>{
+        localStorage.setItem(
+            'react-notes-app-data',
+            JSON.stringify(notes)
+        );
+    },[notes])
 
     const addNote = (text) => {
         const date = new Date();
@@ -41,16 +59,20 @@ const App = () => {
         setNotes(newNotes);
     };
     return (
+        <div className={`${darkMode && 'dark-mode'} `}>
 
-        <div className="container">
-            <Search handleSearchNote={SetsearchText}/>
-            <NotesList
-                 notes={notes.filter((note) => note.text.toLowerCase().includes(searchText)
-                    )} 
-                 handleAddNote={addNote}
-                 handleDeleteNote={deleteNote}
+            <div className="container">
+                <Header handleToggleDarkMode={setDarkMode}/>   
+                <Search handleSearchNote={SetsearchText}/>
+                <NotesList
+                     notes={notes.filter((note) => note.text.toLowerCase().includes(searchText)
+                        )} 
+                    handleAddNote={addNote}
+                    handleDeleteNote={deleteNote}
             />
+            </div>
         </div>
+       
     );
 };
 
---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+
+
+const Header = ({ handleToggleDarkMode }) =>{
+    return (
+        <div className="header">
+            <h1>Notes</h1>
+            <button
+            onClick={() =>
+                handleToggleDarkMode(
+                    (previousDarkMode) => !previousDarkMode
+                )
+            }
+            className="save">Dark mode</button>
+        </div>
+    )
+}
+
+export default Header;
